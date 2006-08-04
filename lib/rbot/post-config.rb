208d91635e9fda96eae0708368352abc61128ef5
Giuseppe Bilotta <giuseppe.bilotta@gmail.com>
2006-08-04 07:15:29 +0000

    Ahem. I should remember to test things ''before'' committing, not after. Fix COREDIR as set by post-config

--- lib/rbot/post-config.rb
+++ lib/rbot/post-config.rb
@@ -3,7 +3,7 @@ File.open("pkgconfig.rb", "w") {|f|
   f.puts "module Irc"
   f.puts "  module PKGConfig"
   f.puts "    DATADIR = '#{config('datadir')}/rbot'"
-  f.puts "    COREDIR = '#{config('rbdir')}/core'"
+  f.puts "    COREDIR = '#{config('rbdir')}/rbot/core'"
   f.puts "  end"
   f.puts "end"
 }
