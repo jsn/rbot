324d51450fcddd1355d4677effd42b4c0e2ded9e
Giuseppe Bilotta <giuseppe.bilotta@gmail.com>
2006-09-09 05:58:08 +0000

    Adapt opme plugin to new auth framework

--- data/rbot/plugins/opme.rb
+++ data/rbot/plugins/opme.rb
@@ -1,19 +1,25 @@
 class OpMePlugin < Plugin
 
   def help(plugin, topic="")
-    return "opme <channel> => grant user ops in <channel>"
+    return "opme [<channel>] => grant user ops in <channel> (or in the current channel if no channel is specified)"
   end
 
-  def privmsg(m)
-    if(m.params)
-      channel = m.params
-    else
-      channel = m.channel
+  def opme(m, params)
+    channel = params[:chan]
+    unless channel
+      if m.private?
+        m.reply "you should tell me where you want me to op you"
+        return
+      else
+        channel = m.channel.to_s
+      end
     end
     target = m.sourcenick
-    m.okay
+    m.okay unless channel == m.channel.to_s
     @bot.sendq("MODE #{channel} +o #{target}")
   end
 end
+
 plugin = OpMePlugin.new
-plugin.register("opme")
+plugin.map("opme [:chan]")
+plugin.default_auth("*",false)
