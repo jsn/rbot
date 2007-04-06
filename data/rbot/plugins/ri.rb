7999621ee6a8a0b0884eae38096f33e983c3a1ad
Dmitry Kim <dmitry point kim at gmail point com>
2007-04-06 14:17:34 +0000

    - (contrib/plugins/ri) removed the old contrib version + (plugins/ri) plugin header

--- data/rbot/plugins/ri.rb
+++ data/rbot/plugins/ri.rb
@@ -1,15 +1,27 @@
+#-- vim:sw=2:et
+#++
+#
+# :title: 'ri' -- ruby documentation plugin
+#
+# Author:  Michael Brailsford  <brailsmt@yahoo.com> aka brailsmt
+# Author:: dmitry kim <dmitry dot kim at gmail dot com>
+# Copyright:: (C) 2007, dmitry kim
+# Copyright:: (C) Michael Brailsford
+# License:: MIT
+#
+
 class RiPlugin < Plugin
+
+  RI_COMMAND = %w{ri -f simple -T}
 
   BotConfig.register BotConfigIntegerValue.new('ri.max_length',
     :default => 512,
     :desc => "Maximum length of ri entry (in bytes) which is ok to be sent to channels")
 
-
-  RI_COMMAND = %w{ri -f simple -T}
-
   def help(plugin, topic="")
     "ri <something> => returns ruby documentation for <something>"
   end
+
   def ri(m, params)
     args = RI_COMMAND.dup
     if a = params[:something]
@@ -34,6 +46,6 @@ class RiPlugin < Plugin
     return
   end
 end
+
 plugin = RiPlugin.new
-plugin.map 'ri :something',
-  :requirements => {:something => /^((-c)|(\w\S+))$/}
+plugin.map 'ri :something', :requirements => {:something => /^((-c)|(\w\S+))$/}
