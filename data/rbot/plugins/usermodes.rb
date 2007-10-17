b60f397a9e92b6e6c8d3c8a5d0ba41a5b8bcb896
Giuseppe Bilotta <giuseppe.bilotta@gmail.com>
2007-10-17 07:52:22 +0000

    usermodes plugin: initial commit. handles automatico usermode setting on connect

---
+++ data/rbot/plugins/usermodes.rb
@@ -0,0 +1,26 @@
+#-- vim:sw=2:et
+#++
+#
+# :title: Usermodes plugin
+#
+# Author:: Giuseppe "Oblomov" Bilotta <giuseppe.bilotta@gmail.com>
+# Copyright:: (C) 2007 Giuseppe Bilotta
+# License:: rbot's licence
+#
+
+class UserModesPlugin < Plugin
+  Config.register Config::StringValue.new('irc.usermodes',
+    :default => '',
+    :desc => "User modes to set when connecting to the server")
+
+  def help(plugin, topic="")
+    "handles automatic usermode settings on connect. See the config variable irc.usermodes"
+  end
+
+  def connect(m)
+    modes = @bot.config['irc.usermodes']
+    @bot.mode(@bot.nick, modes, '') unless modes.empty?
+  end
+end
+
+plugin = UserModesPlugin.new
