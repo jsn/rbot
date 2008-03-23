jsn/rbot

commit 37b6e68426dbe8812b47ae5bcdadf0fb5df76e26

imdb plugin: htmlinfo filter for names too

data/rbot/plugins/imdb.rb
@@ -205,7 +205,8 @@ class Imdb
       return nil if !m
       name = m[1]
 
-      info << "#{name} : http://us.imdb.com#{sr}"
+      info << "#{name}"
+      info << " : http://us.imdb.com#{sr}" unless opts[:nourl]
 
       return info if opts[:name_only]
 
@@ -406,15 +407,29 @@ class ImdbPlugin < Plugin
   attr_reader :i
 
   TITLE_URL = %r{^http://(?:[^.]+\.)?imdb.com(/title/tt\d+/)}
+  NAME_URL = %r{^http://(?:[^.]+\.)?imdb.com(/name/nm\d+/)}
   def imdb_filter(s)
     loc = Utils.check_location(s, TITLE_URL)
     if loc
       sr = loc.first.match(TITLE_URL)[1]
-      extra = $2
+      extra = $2 # nothign for the time being, could be fullcredits or whatever
       res = i.info_title(sr, :nourl => true, :characters => (extra == 'fullcredits'))
       debug res
       if res
         return {:title => res.first, :content => res.last}
+      else
+        return nil
+      end
+    end
+    loc = Utils.check_location(s, NAME_URL)
+    if loc
+      sr = loc.first.match(NAME_URL)[1]
+      extra = $2 # nothing for the time being, could be filmoyear or whatever
+      res = i.info_name(sr, :nourl => true, :movies_by_year => (extra == 'filmoyear'))
+      debug res
+      if res
+        name = res.shift
+        return {:title => name, :content => res.join(". ")}
       else
         return nil
       end

commit 41fba3fa34d46c77dbfb580fc3a8c737d68eafde

imdb plugin: fix author output broken by [37b6e68426dbe8812b47ae5bcdadf0fb5df76e26]

data/rbot/plugins/imdb.rb
@@ -206,7 +206,7 @@ class Imdb
       name = m[1]
 
       info << "#{name}"
-      info << " : http://www.imdb.com#{sr}" unless opts[:nourl]
+      info.last << " : http://www.imdb.com#{sr}" unless opts[:nourl]
 
       return info if opts[:name_only]
 