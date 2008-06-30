0d715cdc29abec6031d3e69968247fb9f973c66d
Diego 'Flameeyes' Pettenò <flameeyes@gmail.com>
2008-06-30 11:39:36 +0200

    Add a host.path setting to set the path to the host command.

--- data/rbot/plugins/host.rb
+++ data/rbot/plugins/host.rb
@@ -1,13 +1,22 @@
 class HostPlugin < Plugin
+  Config.register Config::StringValue.new('host.path',
+     :default => '/usr/bin/host',
+     :desc => _('Path to the host program'))
+
   def help(plugin, topic="")
     "host <domain> => query nameserver about domain names and zones for <domain>"
   end
+
+  def host_path
+    @bot.config["host.path"]
+  end
+
   def privmsg(m)
     unless(m.params =~ /^(\w|-|\.)+$/)
       m.reply "incorrect usage: " + help(m.plugin)
       return
     end
-    m.reply Utils.safe_exec("host", m.params)
+    m.reply Utils.safe_exec(host_path, m.params)
   end
 end
 plugin = HostPlugin.new
