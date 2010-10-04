83229da8d25e044bca03f9ae879cadaf06acae3a
Giuseppe Bilotta <giuseppe.bilotta@gmail.com>
2010-10-04 23:41:59 +0200

    DBFatal at load time deserve special treatment
    Don't trap them, rerise and let the bot handle them

--- lib/rbot/plugins.rb
+++ lib/rbot/plugins.rb
@@ -573,6 +573,7 @@ module Plugins
         return :loaded
       rescue Exception => err
         # rescue TimeoutError, StandardError, NameError, LoadError, SyntaxError => err
+        raise if err.kind_of? DBFatal
         error report_error("#{desc}#{fname} load failed", err)
         bt = err.backtrace.select { |line|
           line.match(/^(\(eval\)|#{fname}):\d+/)
